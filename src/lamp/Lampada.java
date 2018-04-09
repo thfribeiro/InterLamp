f870d5d27104046b23c5d998a001b0087bf44e2a
Thiago Fernandes Ribeiro <thfribeiro@hotmail.com>
2018-04-09 15:07:46 -0300

    Lampada

--- src/lamp/Lampada.java
+++ src/lamp/Lampada.java
@@ -35,7 +35,7 @@ public class Lampada {
         return aceso;
     }
     
-    public String isCor(){
+    public String getCor(){
         return cor;
     }
     
--- src/tela/Tela.java
+++ src/tela/Tela.java
@@ -1,52 +1,69 @@
 package tela;
 
+import java.awt.Color;
+import javax.swing.ImageIcon;
 import lamp.Lampada;
 import javax.swing.JOptionPane;
 
 public class Tela extends javax.swing.JFrame {
     Lampada lampada;
+    ImageIcon lbranca;
+    ImageIcon lverde;
+    ImageIcon lvermelha;
+    ImageIcon lazul;
+    ImageIcon lamarela;
     
     public Tela() {
         lampada = new Lampada();
+        lbranca = new ImageIcon("C:\\Users\\Thiago\\Desktop\\lampadas\\l1.png");
+        lverde = new ImageIcon("C:\\Users\\Thiago\\Desktop\\lampadas\\l4.png");
+        lvermelha = new ImageIcon("C:\\Users\\Thiago\\Desktop\\lampadas\\l5.png");
+        lazul = new ImageIcon("C:\\Users\\Thiago\\Desktop\\lampadas\\l3.png");
+        lamarela = new ImageIcon("C:\\Users\\Thiago\\Desktop\\lampadas\\l2.png");
         initComponents();
     }
 
+    
+     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        jToggleButton1 = new javax.swing.JToggleButton();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
         campoIntensidade = new javax.swing.JTextField();
         campoCor = new javax.swing.JComboBox<>();
         jLabel2 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
-        campoLigada = new javax.swing.JTextField();
+        onoffbutton = new javax.swing.JToggleButton();
+        imagem1 = new javax.swing.JButton();
+        campoLigada = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+
+        jToggleButton1.setText("jToggleButton1");
+
+        jButton1.setText("jButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jLabel1.setText("Lâmpada MEC");
+        jLabel1.setText("Lâmpada");
 
-        jButton1.setText("ON");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
-        jButton2.setText("OFF");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-
+        campoIntensidade.setEditable(false);
         campoIntensidade.setToolTipText("");
+        campoIntensidade.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                campoIntensidadeActionPerformed(evt);
+            }
+        });
 
-        campoCor.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Branca", "Azul", "Verde", "Amarela", "Vermelha", "Rosa", "Negra" }));
+        campoCor.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Branca", "Azul", "Verde", "Amarela", "Vermelha" }));
         campoCor.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 campoCorActionPerformed(evt);
@@ -70,86 +87,126 @@ public class Tela extends javax.swing.JFrame {
             }
         });
 
+        onoffbutton.setText("ON/OFF");
+        onoffbutton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                onoffbuttonActionPerformed(evt);
+            }
+        });
+
+        imagem1.setBackground(new java.awt.Color(255, 255, 255));
+        imagem1.setForeground(new java.awt.Color(255, 255, 255));
+        imagem1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Thiago\\Desktop\\lampadas\\l1.png")); // NOI18N
+        imagem1.setBorderPainted(false);
+
+        campoLigada.setBackground(new java.awt.Color(204, 0, 0));
+        campoLigada.setForeground(new java.awt.Color(204, 0, 0));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
+        jLabel3.setText("Intensidade:");
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Desenvolvido por:");
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Thiago F.R");
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("Willian M.I");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(campoIntensidade, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(imagem1, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(campoCor, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(17, 17, 17)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(campoLigada, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                        .addComponent(campoCor, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(onoffbutton)
+                        .addGap(47, 47, 47))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(29, 29, 29)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel3)
+                                    .addComponent(campoLigada, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(campoIntensidade, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jButton3)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
             .addGroup(layout.createSequentialGroup()
-                .addGap(59, 59, 59)
+                .addGap(96, 96, 96)
                 .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(4, 4, 4)
                 .addComponent(jLabel1)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(4, 4, 4)
-                        .addComponent(campoLigada, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1)
-                        .addGap(16, 16, 16)
-                        .addComponent(jButton2))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jButton3)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton4))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(9, 9, 9)
-                                .addComponent(campoIntensidade, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(22, 22, 22)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
-                            .addComponent(campoCor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(19, Short.MAX_VALUE))
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(onoffbutton, javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(campoCor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(12, 12, 12)
+                        .addComponent(imagem1, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addComponent(campoLigada, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(37, 37, 37)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(campoIntensidade, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(1, 1, 1)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton3)
+                            .addComponent(jButton4))
+                        .addGap(41, 41, 41)
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel6)))
+                .addContainerGap(27, Short.MAX_VALUE))
         );
 
         campoIntensidade.getAccessibleContext().setAccessibleName("campoIntensidade");
-        campoLigada.getAccessibleContext().setAccessibleName("campoLigada");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        lampada.ligar();
-        this.atualizarTela();
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        lampada.desligar();
-        this.atualizarTela();
-    }//GEN-LAST:event_jButton2ActionPerformed
-
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         if (!lampada.isAceso()){
             JOptionPane.showMessageDialog(null, "Ligue a Lampada");
+        }
+        if (lampada.getIntensidade()==100){
+            JOptionPane.showMessageDialog(null, "Intensidade máxima");
         }
         lampada.aumentarIntensidade();
         atualizarTela();
@@ -158,6 +215,7 @@ public class Tela extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         if (!lampada.isAceso()){
             JOptionPane.showMessageDialog(null, "Ligue a Lampada");
+
         }
         lampada.diminuirIntensidade();
         atualizarTela();
@@ -165,20 +223,59 @@ public class Tela extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void campoCorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_campoCorActionPerformed
+          if(lampada.isAceso()){
         lampada.setCor(campoCor.getSelectedItem().toString());
+        if(null != campoCor.getSelectedItem().toString())switch (campoCor.getSelectedItem().toString()) {
+            case "Branca":
+                imagem1.setIcon(lbranca);
+                break;
+            case "Verde":
+                imagem1.setIcon(lverde);
+                break;
+            case "Azul":
+                imagem1.setIcon(lazul);
+                break;
+            case "Vermelha":
+                imagem1.setIcon(lvermelha);
+                break;
+            case "Amarela":
+                imagem1.setIcon(lamarela);
+                break;
+            default:
+                break;
+        }
+
         atualizarTela();
+    }
+          else{
+            JOptionPane.showMessageDialog(null, "Ligue a Lampada");  
+          }
     }//GEN-LAST:event_campoCorActionPerformed
+
+    private void onoffbuttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_onoffbuttonActionPerformed
+        if(lampada.isAceso()==false){
+        lampada.ligar();
+        this.atualizarTela();
+        }
+        else{
+            lampada.desligar();
+            this.atualizarTela();  
+        }
+    }//GEN-LAST:event_onoffbuttonActionPerformed
+
+    private void campoIntensidadeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_campoIntensidadeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_campoIntensidadeActionPerformed
 
     
         public void atualizarTela(){
         if (lampada.isAceso()){
-            campoLigada.setText("Ligada");
+            campoLigada.setBackground(Color.green);
         }
         else{
-            campoLigada.setText("Desligada");
+            campoLigada.setBackground(Color.red);
         }
         this.campoIntensidade.setText(""+lampada.getIntensidade());
-        
     }
     /**
      * @param args the command line arguments
@@ -218,12 +315,18 @@ public class Tela extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> campoCor;
     private javax.swing.JTextField campoIntensidade;
-    private javax.swing.JTextField campoLigada;
+    private javax.swing.JButton campoLigada;
+    private javax.swing.JButton imagem1;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JToggleButton jToggleButton1;
+    private javax.swing.JToggleButton onoffbutton;
     // End of variables declaration//GEN-END:variables
 }
